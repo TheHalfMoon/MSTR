530b384cbfc4fa37538970a1dad37013c4736f32
Abdulaziz M. Shehri <alshehriofficial@gmail.com>
2026-08-31 19:40:19 +0300

    feat(mstr-000a): export A013 admission primitives

--- src/mstr_qualify/environment/__init__.py
+++ src/mstr_qualify/environment/__init__.py
@@ -1,5 +1,14 @@
-"""MSTR environment reset/setup primitives."""
+"""MSTR environment reset/setup/admission primitives."""
 
+from .admission import (
+    AdmissionAttemptRecord,
+    EnvironmentAdmissionError,
+    EnvironmentAdmissionRecord,
+    EnvironmentHealthCheckError,
+    HealthCheckResult,
+    IndependentHealthChecker,
+    admit_environment,
+)
 from .reset import (
     CommandResult,
     EffectEnvelope,
@@ -14,14 +23,21 @@ from .reset import (
 )
 
 __all__ = [
+    "AdmissionAttemptRecord",
     "CommandResult",
     "EffectEnvelope",
+    "EnvironmentAdmissionError",
+    "EnvironmentAdmissionRecord",
+    "EnvironmentHealthCheckError",
     "EnvironmentResetError",
     "EnvironmentSetupRecord",
     "ExecutorEnvelope",
     "FreshCloneDriver",
+    "HealthCheckResult",
+    "IndependentHealthChecker",
     "ResourceEnvelope",
     "SetupExecutor",
     "SetupStepRecord",
+    "admit_environment",
     "prepare_environment",
 ]
