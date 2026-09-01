18bbcee28ae0ec3a35547e9c1e446c96dc02c040
Abdulaziz M. Shehri <alshehriofficial@gmail.com>
2026-09-01 18:13:43 +0300

    feat(verifier): export B023 health evaluator

--- src/mstr_qualify/verifier/__init__.py
+++ src/mstr_qualify/verifier/__init__.py
@@ -1,6 +1,13 @@
-"""Protected verifier/finalizer boundary for MSTR-000A."""
+"""Protected verifier/finalizer boundary for MSTR-000A and B023 health evaluation."""
 
 from .finalizer import FinalizationDecision, FinalizerError, finalize_run
+from .health import (
+    HealthClass,
+    VerifierHealthEvaluationError,
+    VerifierHealthObservation,
+    classify_verifier_health,
+    evaluate_verifier_health,
+)
 from .runner import (
     REQUIRED_SHORTCUT_CLASSES,
     BatteryCaseResult,
@@ -23,13 +30,18 @@ __all__ = [
     "ControlledVerifierExecutor",
     "FinalizationDecision",
     "FinalizerError",
+    "HealthClass",
     "REQUIRED_SHORTCUT_CLASSES",
     "RewardShortcutBatteryRecord",
     "ShortcutClass",
     "VerifierFixtureCase",
+    "VerifierHealthEvaluationError",
+    "VerifierHealthObservation",
     "VerifierResult",
     "VerifierRunRecord",
     "VerifierRunnerError",
+    "classify_verifier_health",
+    "evaluate_verifier_health",
     "finalize_run",
     "hash_path",
     "run_reward_shortcut_battery",
